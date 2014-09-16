0023dd28db30a42a91132e2c9558b4cd84e6a5b8
Masaki Iitsuka <iizuka@i-fc.jp>
2014-09-16 13:33:07 +0900

    テストアップロード

---
+++ NewWorld-ja/newworld/footer.php
@@ -0,0 +1,21 @@
+				<br class="clear">
+			</div><!-- /#contents -->
+			
+<!-- Footer -->			
+			<div id="footer">
+			
+				<?php dynamic_sidebar( 'footer-widget' ); ?>
+				
+				<p class="copy">
+					&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. All rights reserved. 
+					Theme Design by <a href="http://www.webcreatormana.com/">Mana</a> 
+					from <a href="http://www.webcreatorbox.com/">Webクリエイターボックス</a>
+				</p>
+
+			</div><!-- /#footer -->
+		</div><!-- /#wrapper -->
+	</div><!-- /#page -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
